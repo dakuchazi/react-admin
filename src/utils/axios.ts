6095db272e9f128be35a2ecf653d85b92026e98a
dakuchazi <2320818066@qq.com>
2024-04-25 11:35:40 +0800

    init

---
+++ src/utils/axios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+
+const instance = axios.create({})
+
+//请求拦截器
+instance.interceptors.request.use()
+
+
+
+//响应拦截器
+instance.interceptors.response.use()
+
+
+export default instance
